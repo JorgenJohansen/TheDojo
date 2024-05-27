c01b35a6afea4473c5dccd5b54e3dfbe64b83121
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2024-05-27 13:04:33 +0200

    Added useDocument hook and project summary

---
+++ src/pages/project/ProjectSummary.js
@@ -0,0 +1,11 @@
+export default function ProjectSummary({ project }) {
+  return (
+    <div className="project-summary">
+        <h2 className="page-title">{project.name}</h2>
+        <p className="due-date">
+            Project due by {project.dueDate.toDate().toDateString()}
+        </p>
+        <p className="details"></p>
+    </div>
+  )
+}
